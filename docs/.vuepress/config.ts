bb85a2f7c8cc00c9ecbcfebd7ecc854b181b8368
LuoXiaoDong <landonniao@163.com>
2022-10-31 11:26:09 +0800

    fix:处理业务问题

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,7 +15,7 @@ export default defineUserConfig({
   description: '能够灵活满足企业数字化建设中各种场景的需要，更高效、专注的沉淀业务和数据能力，进而形成企业的业务和数据中台。通过能力的灵活组合，快速的应对当前快节奏的市场需求，助力企业数字化转型的成功。',
   base: '/',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.png' }]
+    ['link', { rel: 'icon', href: '/favicon.png' }] ,
     ['link', { rel: 'stylesheet', href: 'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.11.2/css/all.css' }]
   ],
   bundler: viteBundler({
