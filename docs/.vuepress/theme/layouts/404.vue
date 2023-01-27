239ed01d0138e62d57dbd45bbb3013334cda6068
weixiaojin0712 <luoandong@gmail.com>
2023-01-27 10:31:20 +0800

    feat:完善项目配置

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -5,8 +5,6 @@
 
       <blockquote>{{ getMsg() }}</blockquote>
 
-      <div class="show-time-box">{{ count + 1 }} s 后将自动跳转到首页</div>
-
       <RouterLink to="/">
         Take me home.
       </RouterLink>
@@ -28,44 +26,10 @@ export default {
       count: 0 ,//倒计时
     }
   },
-
-  created() {
-    this.threeGo()
-  },
-
   methods: {
-    //3秒后进入跳转页面
-    threeGo() {
-      const TIME_COUNT = 4;
-      if (!this.timer) {
-        this.count = TIME_COUNT;
-        this.show = false;
-        this.timer = setInterval(() => {
-          if (this.count > 0 && this.count <= TIME_COUNT) {
-            this.count--;
-          } else {
-            this.show = true;
-            clearInterval(this.timer);
-            this.timer = null;
-            //跳转的页面写在此处
-            this.$router.push({
-              path: '/'
-            });
-          }
-        }, 1000)
-      }
-    },
     getMsg() {
       return msgs[Math.floor(Math.random() * msgs.length)]
     }
   }
 }
 </script>
-
-<style lang="stylus" scoped>
-.show-time-box {
-  margin-top: 20px;
-  margin-bottom: 20px;
-  margin-left: 5px ;
-}
-</style>
